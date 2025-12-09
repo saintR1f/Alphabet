6319d30a6a100f06d34b9919288f65f930e0e87d
Житникович Максим Михайловчи <zhitnikovich_m@NLA.LOCAL>
2025-12-09 11:20:23 +0300

    Init: Create project

---
+++ src/components/Alphabet.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+const str: string = "abcdefghijklmnopqrstuvwxyz"
+
+
+
+export default defineComponent({
+  name: "LetterCard"
+
+})
+</script>
